100989d2dd3a6b7f9064411ac3b9aaa1a4706a71
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-02-17 19:38:55 +0100

    add API support, scraping abilities, can read data from instagram API

--- app/controllers/toptens_controller.rb
+++ app/controllers/toptens_controller.rb
@@ -1,9 +1,41 @@
+require "json"
+require "open-uri"
+require "nokogiri"
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class ToptensController < ApplicationController
   before_action :set_topten, only: [:show, :edit, :update, :destroy]
 
   # GET /toptens
   def index
     @toptens = Topten.all
+
+    # url = "http://tmdb.lewagon.com/movie/top_rated"
+    # movie_serialized = URI.open(url).read
+    # @movies = JSON.parse(movie_serialized)
+
+    # url = "https://www.claudia-maechtle.de/"
+    # url = "https://www.google.de/maps/place/U+Heinrich-Heine-Stra%C3%9Fe/@52.5086709,13.4131792,15z/data=!4m5!3m4!1s0x47a84e25398bf107:0xa04b3e09a0d4d96a!8m2!3d52.510858!4d13.4163621"
+    
+    # html_file = URI.open(url).read
+    # @html_doc = Nokogiri::HTML(html_file)
+    username = "liobaer"
+    user_id = "237266180"
+    # url = URI("https://instagram47.p.rapidapi.com/get_user_id?username=#{username}")
+    # url = URI("https://instagram47.p.rapidapi.com/user_followers?userid=#{user_id}")
+    url = URI("https://instagram47.p.rapidapi.com/email_and_details?userid=#{user_id}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'instagram47.p.rapidapi.com'
+    request["x-rapidapi-key"] = '0363c85d04msh3370cfb927787c3p1afecdjsn2b8cab3ddff8'
+
+    @insta = http.request(request)
   end
 
   # GET /toptens/1
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Topten.destroy_all
+
+puts "Creating Top Ten Lists..."
+u_station = { name: "Heinrich-Heine-Strasse", comment: "has no elevator" address: "www.somestation.de", stars: 4 }
+s_station =  { name: "Jannowitz Brucke", comment: "is being renovated" address: "www.station.de", stars: 3 }
+
+[ u_station, s_station ].each do |attributes|
+  topten = Topten.create!(attributes)
+  puts "Created #{topten.name}"
+end
+puts "Finished!"
